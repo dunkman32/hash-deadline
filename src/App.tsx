584356b3f1b781f8f0423250087492089679e2e8
Zura Nozadze <36369798+dunkman32@users.noreply.github.com>
2023-11-20 11:57:50 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import Countdown from "react-countdown";
+
+import "./App.css";
+
+const fetchKey = async () => {
+  const res = await fetch(
+    "https://api.jsonbin.io/v3/b/655b047512a5d376599bf9f5",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-ACCESS-KEY":
+          "$2a$10$8DmwLsCp0SQAaT8glh0/Cey/zi5Rj4wwHIoUXe7608Jh6f3ulF49e",
+      },
+    }
+  );
+
+  if (res.ok) {
+    return res.json();
+  } else {
+    return {
+      record: {
+        hash: 1722456000000,
+      },
+    };
+  }
+};
+
+function App() {
+  const [date, setDate] = useState<number>(0);
+
+  useEffect(() => {
+    fetchKey().then((r) => {
+      if (r?.record?.hash) {
+        console.log(123);
+        setDate(r.record.hash);
+      }
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <div className="main">
+        <img className="image" src="deadline.png" alt="deadline" />
+        {date ? (
+          <Countdown
+            date={new Date(date)}
+            renderer={(props) => {
+              console.log(props);
+              return (
+                <div
+                  style={{
+                    display: "flex",
+                    gap: 16,
+                    alignItems: "center",
+                  }}
+                >
+                  <div className="wrap">
+                    <p className="title">Days</p>
+                    <p className="value">{props.days}</p>
+                  </div>
+                  :
+                  <div className="wrap">
+                    <p className="title">Hours</p>
+                    <p className="value">{props.hours}</p>
+                  </div>
+                  :
+                  <div className="wrap">
+                    <p className="title">Minutes</p>
+                    <p className="value">{props.minutes}</p>
+                  </div>
+                  :
+                  <div className="wrap">
+                    <p className="title">Seconds</p>
+                    <p className="value">{props.seconds}</p>
+                  </div>
+                </div>
+              );
+            }}
+          />
+        ) : (
+          <p className="title">loading ...</p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
